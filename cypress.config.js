4cb33ce358b4fe2a082c0ecd0fd749a1732cf554
Taavi <taavi.taivere@khk.ee>
2023-02-05 14:42:41 +0200

    Testing actions

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  env: {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijoic3R1ZmYiLCJpYXQiOjE2NzM3MzUzMTR9.14xgyGF8D_0MJALNSUfKpm2sCZ_H8xJdyBpFsypbx6o",
-  },
 });
