31ae483f9b2bb5efa354a89ce0d01b526d64c09a
Taavi <taavi.taivere@khk.ee>
2023-02-05 15:07:40 +0200

    Testing actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  supportFile: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
