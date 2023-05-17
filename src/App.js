c813a820ebe09286eb81839e1f37cd8ca9528339
Taavi <taavi.taivere@khk.ee>
2023-05-17 21:23:36 +0300

    Added create person form component to the application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
-import { BrowserRouter, Link } from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import CreatePerson from "./components/CreatePerson";
 import React from "react";
 
 function App() {
 
-  return (
-      <div className="container">
-        <div className="App">
-          <h1>React CRUD</h1>
-            <BrowserRouter>
-                    <Link className="btn btn-primary main-buttons" name="create">Create Person</Link>
+    return (
+        <div className="container">
+            <div className="App">
+                <h1>React CRUD</h1>
+                <BrowserRouter>
+                    <Link to="person/create" className="btn btn-primary main-buttons" name="create">Create Person</Link>
                     <Link className="btn btn-primary main-buttons" name="list">List Person</Link>
                     <Link className="btn btn-primary main-buttons" name="logs">Log List</Link>
-            </BrowserRouter>
+                    <Routes>
+                        <Route path="/person/create" element={<CreatePerson/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
