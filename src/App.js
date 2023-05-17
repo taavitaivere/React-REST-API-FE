a207c542dc342cdf9b7b093a6edfc5d22c4a8916
Taavi <taavi.taivere@khk.ee>
2023-05-17 20:55:30 +0300

    adding main buttons for application

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
 import React from "react";
 
 function App() {
@@ -6,7 +8,11 @@ function App() {
       <div className="container">
         <div className="App">
           <h1>React CRUD</h1>
-
+            <BrowserRouter>
+                    <Link className="btn btn-primary main-buttons" name="create">Create Person</Link>
+                    <Link className="btn btn-primary main-buttons" name="list">List Person</Link>
+                    <Link className="btn btn-primary main-buttons" name="logs">Log List</Link>
+            </BrowserRouter>
         </div>
       </div>
   );
