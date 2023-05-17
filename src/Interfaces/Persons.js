8d2e21cb699a5d168a8876173e63721b08cb56f6
Taavi <taavi.taivere@khk.ee>
2023-05-17 22:00:27 +0300

    Added persons interface for application

---
+++ src/Interfaces/Persons.js
@@ -0,0 +1,18 @@
+export interface personData {
+    id: number;
+    name: string;
+    email: string;
+    avatar: string;
+}
+
+export interface editedPersonData {
+    name: string;
+    email: string;
+    avatar: string;
+}
+
+export interface newPersonData {
+    name: string;
+    email: string;
+    avatar: string;
+}
