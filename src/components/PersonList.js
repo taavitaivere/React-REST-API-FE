cb9cc0c38bd9748dfd2f1a8597146b352207e2de
Taavi <taavi.taivere@khk.ee>
2023-05-17 22:19:45 +0300

    Added public API integration for person list component to view created persons

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,4 +1,25 @@
-export default function PersonList( ) {
+import axios from 'axios';
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+export default function PersonList() {
+    const navigate = useNavigate();
+    const [persons, setPersons] = useState([])
+
+    useEffect(() => {
+        getPersonList();
+    }, []);
+
+    function getPersonList() {
+        axios.get('https://63274caeba4a9c475334aec1.mockapi.io/crud')
+            .then(function (response) {
+                setPersons(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div className="row">
             <div className="col-md-12">
@@ -13,7 +34,15 @@ export default function PersonList( ) {
                         </tr>
                         </thead>
                         <tbody>
-                        </tbody>
+                    {persons.map((persons, index) => (
+                            <tr key={index}>
+                                <td>{persons.id}</td>
+                                <td>{persons.name}</td>
+                                <td>{persons.email}</td>
+                                <td><img className="img" src={persons.avatar} alt=""/></td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </div>
