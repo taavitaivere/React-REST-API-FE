b7a8b6cf884cf3f611873b8386487c5bfc6822ac
Taavi <taavi.taivere@khk.ee>
2023-05-17 22:03:34 +0300

    Added public API integration for create person component

--- src/components/CreatePerson.js
+++ src/components/CreatePerson.js
@@ -1,21 +1,44 @@
-export default function PersonList() {
+import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+
+export default function PersonList( ) {
+
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState([]);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('https://63274caeba4a9c475334aec1.mockapi.io/crud', inputs).then(function (response) {
+        })
+        navigate('/');
+    }
 
     return (
         <div className="row">
             <div className="col-md-12">
                 <h1>Create Person</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" name="name" id="name"/>
+                        <input type="text" className="form-control" name="name" id="name" onChange={handleChange}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" className="form-control" name="email" id="email"/>
+                        <input type="email" className="form-control" name="email" id="email" onChange={handleChange}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="avatar" className="form-label">Avatar</label>
-                        <input type="text" className="form-control" name="avatar" id="avatar"/>
+                        <input type="text" className="form-control" name="avatar" id="avatar" onChange={handleChange}/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
